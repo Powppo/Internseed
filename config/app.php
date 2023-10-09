15d244417916cdcf67e084f210913f038d25374e
ngagamx <mafrizal575@gmail.com>
2023-10-09 11:43:53 +0700

    forum 80% navbar after discussion issues

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        DevDojo\Chatter\ChatterServiceProvider::class,
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
     ])->toArray(),
 
     /*
