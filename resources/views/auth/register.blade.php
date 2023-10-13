7789aba4cdbe82645b6bdb96e74860df728c1aad
ngagamx <ngagamx@gmail.com>
2023-10-13 08:18:06 +0700

    fe

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,16 +3,31 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
+    <div class="col-md-4">
+            <img src="{{ asset('public/images/register.jpg') }}"  class="img-fluid">
+        </div>
+
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card">     
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
+                        <div class="tt-title">
+                            Buat Akun
+                        </div>
+
+
+                        <div class="tt-description">
+                        Gabung dengan Komunitas Kami dan termukan banyak Peluang Magang
+                        </div>
+
+                        <div class="custom-margin"></div>
+
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -61,15 +76,26 @@
                             </div>
                         </div>
 
+                        <div class="title">
+                            Buat Akun Sebagai:
+                        </div>
+                        <div class="custom-margin"></div>
+
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Daftar') }}
                                 </button>
                             </div>
                         </div>
 
-                        <!-- Continue Google -->
+                        <div class="custom-margin"></div>
+
+
+                        <p>Sudah punya  akun? <a href="{{ route('login') }}" class="tt-underline">Masuk</a></p>
+
+                        <!-- Continue Google 
                         <div class="row">
                             <div class="container" style="text-align: center">
                                 <div style="float:left; width: 48%;"><hr/></div>
@@ -77,7 +103,7 @@
                                 <div>Or</div>
                               <a  class="btn btn-md btn-google btn-block btn-outline" href="{{ '/auth/redirect'}}"><img style="width:13%" src="{{url('/images/googleicon.png')}}"> Continue with Google</a>
                             </div>
-                        </div>
+                        </div> -->
 
                     </form>
                 </div>
